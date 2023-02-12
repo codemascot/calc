6cb623e8601f03c9953f14cd67b6b65726ee739b
Khan Mohammad R <codemascot@hotmail.com>
2023-02-12 18:48:36 +0100

    Data layer decoupled to new namespace

--- src/calc/core.clj
+++ src/calc/core.clj
@@ -3,41 +3,15 @@
             [reitit.ring :as r]
             [muuntaja.core :as m]
             [reitit.ring.middleware.muuntaja :as mu]
-            [calc.util :as util]
-            [calc.algo :as algo])
+            [calc.data :as d])
   (:gen-class))
-
-(def history (atom {}))
-
-(defn add-to-history [key exp value]
-  (swap! history update-in [key] assoc :expression exp :result value))
-
-(defn get-result
-  [exp]
-  (let [h (keyword (str (util/md5 exp)))]
-    (if (contains? @history h)
-      (:result (h @history))
-      (let [result (eval (algo/infix-to-prefix (util/parse exp)))]
-        (add-to-history h exp result)
-        result))))
-
-(defn eval-exp [{payload :body-params}]
-  (let [p (:expression payload)
-        result (get-result p)]
-    {:status 200
-     :body {:result result}}))
-
-(defn get-history
-  [_]
-  {:status 200
-   :body @history})
 
 (def app
   (r/ring-handler
    (r/router
     ["/"
-     ["calc" {:get get-history
-              :post eval-exp}]
+     ["calc" {:get d/get-history
+              :post d/eval-exp}]
      ["" (fn [_] {:status 200
                   :body "Nothing Here"})]]
     {:data {:muuntaja m/instance
---
+++ src/calc/data.clj
@@ -0,0 +1,28 @@
+(ns calc.data
+  (:require [calc.util :as util]
+            [calc.algo :as algo]))
+
+(def history (atom {}))
+
+(defn add-to-history [key exp value]
+  (swap! history update-in [key] assoc :expression exp :result value))
+
+(defn get-result
+  [exp]
+  (let [h (keyword (str (util/md5 exp)))]
+    (if (contains? @history h)
+      (:result (h @history))
+      (let [result (eval (algo/infix-to-prefix (util/parse exp)))]
+        (add-to-history h exp result)
+        result))))
+
+(defn eval-exp [{payload :body-params}]
+  (let [p (:expression payload)
+        result (get-result p)]
+    {:status 200
+     :body {:result result}}))
+
+(defn get-history
+  [_]
+  {:status 200
+   :body @history})
